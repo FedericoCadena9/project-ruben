c41fbee0eef6015eb38b9582d37113bcd57afb1a
Federico Cadena <73864330+FedericoCadena9@users.noreply.github.com>
2022-06-01 15:50:06 -0500

    Fix Sintax

--- components/Upload.jsx
+++ components/Upload.jsx
@@ -76,10 +76,10 @@ export default function Upload({ isOpen, closeModal }) {
                       />
                     </div>
 
-                    <div class="w-full space-y-1">
+                    <div className="w-full space-y-1">
                       <label
-                        for="basic"
-                        class="text-sm font-semibold text-gray-600"
+                        htmlFor="basic"
+                        className="text-sm font-semibold text-gray-600"
                       >
                         Descripción
                       </label>
@@ -87,14 +87,14 @@ export default function Upload({ isOpen, closeModal }) {
                         rows="4"
                         id="basic"
                         placeholder="Describe tu proyecto"
-                        class="block w-full rounded-md border-gray-200 text-sm transition focus:border-blue-600 focus:ring-blue-600 disabled:cursor-not-allowed disabled:bg-gray-200 disabled:opacity-75"
+                        className="block w-full rounded-md border-gray-200 text-sm transition focus:border-blue-600 focus:ring-blue-600 disabled:cursor-not-allowed disabled:bg-gray-200 disabled:opacity-75"
                       ></textarea>
                     </div>
 
-                    <div class="w-full space-y-1">
+                    <div className="w-full space-y-1">
                       <label
                         for="basic"
-                        class="text-sm font-semibold text-gray-600"
+                        className="text-sm font-semibold text-gray-600"
                       >
                         Requisitos
                       </label>
@@ -102,43 +102,43 @@ export default function Upload({ isOpen, closeModal }) {
                         rows="4"
                         id="basic"
                         placeholder="Requsitos del proyecto..."
-                        class="block w-full rounded-md border-gray-200 text-sm transition focus:border-blue-600 focus:ring-blue-600 disabled:cursor-not-allowed disabled:bg-gray-200 disabled:opacity-75"
+                        className="block w-full rounded-md border-gray-200 text-sm transition focus:border-blue-600 focus:ring-blue-600 disabled:cursor-not-allowed disabled:bg-gray-200 disabled:opacity-75"
                       ></textarea>
                     </div>
 
                     <div className="py-4">
                       <label
-                        class="flex w-full cursor-pointer appearance-none justify-center rounded-md border border-dashed border-gray-300 bg-white px-3 py-6 text-sm transition hover:border-gray-400 focus:border-solid focus:border-blue-600 focus:outline-none focus:ring-1 focus:ring-blue-600 disabled:cursor-not-allowed disabled:bg-gray-200 disabled:opacity-75"
-                        tabindex="0"
+                        className="flex w-full cursor-pointer appearance-none justify-center rounded-md border border-dashed border-gray-300 bg-white px-3 py-6 text-sm transition hover:border-gray-400 focus:border-solid focus:border-blue-600 focus:outline-none focus:ring-1 focus:ring-blue-600 disabled:cursor-not-allowed disabled:bg-gray-200 disabled:opacity-75"
+                        tabIndex="0"
                       >
                         <span
-                          for="photo-dropbox"
-                          class="flex items-center space-x-2"
+                          htmlFor="photo-dropbox"
+                          className="flex items-center space-x-2"
                         >
                           <svg
-                            class="h-6 w-6 stroke-gray-400"
+                            className="h-6 w-6 stroke-gray-400"
                             viewBox="0 0 256 256"
                           >
                             <path
                               d="M96,208H72A56,56,0,0,1,72,96a57.5,57.5,0,0,1,13.9,1.7"
                               fill="none"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="24"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="24"
                             ></path>
                             <path
                               d="M80,128a80,80,0,1,1,144,48"
                               fill="none"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="24"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="24"
                             ></path>
                             <polyline
                               points="118.1 161.9 152 128 185.9 161.9"
                               fill="none"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="24"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="24"
                             ></polyline>
                             <line
                               x1="152"
@@ -146,19 +146,19 @@ export default function Upload({ isOpen, closeModal }) {
                               x2="152"
                               y2="128"
                               fill="none"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="24"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="24"
                             ></line>
                           </svg>
-                          <span class="text-xs font-medium text-gray-600">
+                          <span className="text-xs font-medium text-gray-600">
                             Arrastra tu imagen o{" "}
-                            <span class="text-blue-600 underline">
+                            <span className="text-blue-600 underline">
                               busca aquí
                             </span>
                           </span>
                         </span>
-                        <input id="photo-dropbox" type="file" class="sr-only" />
+                        <input id="photo-dropbox" type="file" className="sr-only" />
                       </label>
                     </div>
                   </form>
