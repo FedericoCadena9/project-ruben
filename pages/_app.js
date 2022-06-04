9314dcb3c89d0a53eceace1db465e8c5dc41e576
Federico Cadena <73864330+FedericoCadena9@users.noreply.github.com>
2022-06-03 22:31:55 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -33,7 +33,7 @@ MyApp.getInitialProps = async ({ctx}) => {
 
   // Condicion que preguntas si existe el jwt, en caso de que no manda al Login a todas las rutas que se presenten en la condición
   if(!jwt) {
-    if (ctx.pathname === "/") {
+    if (ctx.pathname === "/" || ctx.pathname === "/project") {
       redirectUser(ctx, "/login");
     }
   }
