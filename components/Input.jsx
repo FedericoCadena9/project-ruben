d157227e61a8196bfc9a4446f87cab794d080676
Federico Cadena <73864330+FedericoCadena9@users.noreply.github.com>
2022-06-04 23:23:47 -0500

    Add name property input component

--- components/Input.jsx
+++ components/Input.jsx
@@ -1,4 +1,4 @@
-export const Input = ({id, type, label, placeholder, value, onChange}) => {
+export const Input = ({id, type, label, placeholder, value, onChange, name}) => {
   return (
     <div className="w-full space-y-2">
       <label htmlFor={id} className="text-sm font-semibold text-gray-600">
@@ -8,6 +8,7 @@ export const Input = ({id, type, label, placeholder, value, onChange}) => {
         id={id}
         type={type}
         value={value}
+        name={name}
         onChange={onChange}
         placeholder={placeholder}
         className="block w-full h-11 rounded-md placeholder:text-gray-400 border-gray-300 text-sm transition focus-within:text-gray-800 focus:border-blue-600 focus:ring-blue-600 disabled:cursor-not-allowed disabled:bg-gray-200 disabled:opacity-75"
