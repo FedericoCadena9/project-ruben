f821dcde8c37c9b4af77e2f90ab99889c89cfba9
R0gerU <60295354+R0gerU@users.noreply.github.com>
2022-06-05 20:49:45 -0500

    register add data

--- pages/register.jsx
+++ pages/register.jsx
@@ -1,9 +1,46 @@
+//import toast, {Toaster} from "react-hot-toast";
 import Image from "next/image";
 import Link from 'next/link';
 import Head from "next/head";
 import { Input } from "../components/Input";
+import { useState } from "react";
+import axios from 'axios';
 
-const Register = () => {
+
+
+const Register = () => {    
+  const [usuarioCorreo, setUsuarioCorreo] = useState("");
+  const [usuarioTelefono, setUsuarioTelefono] = useState("");
+  const [usuarioContrasenia, setUsuarioContrasenia] = useState("");
+
+  async function addUser(){
+    /*const usuariosInfo ={
+    correo: usuarioCorreo,
+    telefono: usuarioTelefono,
+    };*/
+    const { data } = await axios.post(
+      'https://software-ing.herokuapp.com/api/usuarios',
+      {
+        data: {
+          correo: setUsuarioCorreo,
+          telefono: setUsuarioTelefono,
+        }
+      },
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization:
+            'Bearer 410d6ce4c4c1d5f914387fdea72ba9f99be3ebcb0386cb85fe0c90cb5fd383a548a32a40b977cab34b05616a6ab1f566cfe67e7901e8d43916f8a388a376e7ed6b44b30d5185d154d1e2012077befcf844bc7d57ccaf74daa80f45fe2fbcfcec5049a9647e8e34e26da3b34f8d9d697c14df028614abbd60fd3408ec209312e9',
+        },
+        body: JSON.stringify(data),
+      }
+      
+    );
+    //const add = await add.json();
+    console.log(data);
+    };
   return (
     <section className="min-h-screen flex items-center justify-center">
       <div className="w-full lg:px-3 xl:px-0 place-self-center px-4 sm:px-0 sm:max-w-md">
@@ -49,6 +86,8 @@ const Register = () => {
             <Input
               id="correo"
               type="email"
+              onChange={(e)=>setUsuarioCorreo(e.target.value)}
+              value={usuarioCorreo}
               label="Correo Electrónico"
               placeholder="a12345678@correo.edu.mx"
             />
@@ -57,6 +96,8 @@ const Register = () => {
             <Input
               id="telefono"
               type="text"
+              onChange={(e)=>setUsuarioTelefono(e.target.value)}
+              value={usuarioTelefono}
               label="Teléfono (Opcional)"
               placeholder="555 666 7777"
             />
@@ -65,6 +106,8 @@ const Register = () => {
             <Input
               id="password"
               type="password"
+              onChange={(e)=>setUsuarioContrasenia(e.target.value)}
+              value={usuarioContrasenia}
               label="Contraseña"
               placeholder="Al menos 8 caracteres"
             />
@@ -80,7 +123,7 @@ const Register = () => {
 
           <div className="flex items-center justify-center my-10">
             <Link href="/registerPersonal">
-              <a className="btn-primary">Siguiente</a>
+              <a className="btn-primary" onClick={()=>addUser()}>Siguiente</a>
             </Link>
           </div>
         </form>
@@ -96,5 +139,4 @@ const Register = () => {
     </section>
   )
 }
-
-export default Register
+export default Register;
--- pages/registerPersonal.jsx
+++ pages/registerPersonal.jsx
@@ -2,8 +2,49 @@ import Image from "next/image";
 import Link from 'next/link';
 import Head from "next/head";
 import { Input } from "../components/Input";
+import {useState} from "react";
+import axios from 'axios';
+
 
 const RegisterPersonal = () => {
+    const [usuarioNombre, setUsuarioNombre] = useState("");
+    const [usuarioApellidoPaterno, setUsuarioApellidoPaterno] = useState("");
+    const [usuarioApellidoMaterno, setUsuarioApellidoMaterno] = useState("");
+    const [usuarioCarrera, setUsuarioCarrera] = useState("");
+    const [usuarioSemestre, setUsuarioSemestre] = useState("");
+
+    async function addUser(){
+        /*const usuariosInfo ={
+            nombre: usuarioNombre,
+            apellido_Paterno: usuarioApellidoPaterno,
+            apellido_Materno: usuarioApellidoMaterno,
+            carrera: usuarioCarrera,
+            semestre: usuarioSemestre
+        };*/
+
+        const { data } = await axios.post(
+          'https://software-ing.herokuapp.com/api/usuarios',
+          {
+            data: {
+                nombre: setUsuarioNombre,
+                apellido_paterno: setUsuarioApellidoPaterno,
+                apellido_materno: setUsuarioApellidoMaterno,
+                carrera: setUsuarioCarrera,
+                semestre: setUsuarioSemestre,
+            }
+          },
+          {
+            headers: {
+              Authorization:
+                '410d6ce4c4c1d5f914387fdea72ba9f99be3ebcb0386cb85fe0c90cb5fd383a548a32a40b977cab34b05616a6ab1f566cfe67e7901e8d43916f8a388a376e7ed6b44b30d5185d154d1e2012077befcf844bc7d57ccaf74daa80f45fe2fbcfcec5049a9647e8e34e26da3b34f8d9d697c14df028614abbd60fd3408ec209312e9',
+            },
+            //body: JSON.stringify(usuariosInfo),
+          }
+          
+        );
+        //const add = await add.json();
+        console.log(data);
+        };
     return (
         <section className="min-h-screen flex items-center justify-center">
             <div className="w-full max-w-sm lg:px-3 xl:px-0 place-self-center px-4 sm:px-0 sm:max-w-md">
@@ -42,11 +83,13 @@ const RegisterPersonal = () => {
                     </h1>
                 </div>
                 <form className="mt-6">
-                    <div className="space-y-4">
+                    <div className="space-y-4" >
                         {/* Nombres */}
                         <Input
                             id="nombres"
                             type="text"
+                            onChange={(e)=>setUsuarioNombre(e.target.value)}
+                            value={usuarioNombre}
                             label="Nombre(s)"
                             placeholder="Jhon"
                         />
@@ -56,12 +99,16 @@ const RegisterPersonal = () => {
                             <Input
                                 id="apellidoPaterno"
                                 type="text"
+                                onChange={(e)=>setUsuarioApellidoPaterno(e.target.value)}
+                                value={usuarioApellidoPaterno}
                                 label="Apellido Paterno"
                                 placeholder="Doe"
                             />
                             <Input
                                 id="apellidoMaterno"
                                 type="text"
+                                onChange={(e)=>setUsuarioApellidoMaterno(e.target.value)}
+                                value={usuarioApellidoMaterno}
                                 label="Apellido Materno"
                                 placeholder="Smith"
                             />
@@ -70,9 +117,14 @@ const RegisterPersonal = () => {
                         {/* Programa Educativo */}
                         <div className="w-full space-y-2">
                             <label htmlFor="programaEducativo" className="text-sm font-semibold text-gray-600"> Programa Educativo </label>
-                            <select id="programaEducativo" name="programaEducativo" className="block w-full h-11 rounded-md text-gray-400 border-gray-300 text-sm transition focus-within:text-gray-800 focus:border-blue-600 focus:ring-blue-600 disabled:cursor-not-allowed disabled:bg-gray-200 disabled:opacity-75">
+                            <select id="programaEducativo" name="programaEducativo" className="block w-full h-11 rounded-md text-gray-400 border-gray-300 text-sm transition focus-within:text-gray-800 focus:border-blue-600 focus:ring-blue-600 disabled:cursor-not-allowed disabled:bg-gray-200 disabled:opacity-75" onChange={(e)=>setUsuarioCarrera(e.target.value)} value={usuarioCarrera}>
                                 <option defaultValue="">Elegir Programa Educativo</option>
                                 <option>Sistemas Computacionales</option>
+                                <option>Energias Renovables</option>
+                                <option>Ingeniería Industrial</option>
+                                <option>Arquitectura</option>
+                                <option>Gestión Empresarial</option>
+                                <option>Ingeniería en Administración</option>
                             </select>
                         </div>
 
@@ -80,16 +132,26 @@ const RegisterPersonal = () => {
                         {/* Semestre  */}
                         <div className="w-full space-y-2">
                             <label htmlFor="semestre" className="text-sm font-semibold text-gray-600"> Semestre </label>
-                            <select id="semestre" name="semestre" className="block w-full h-11 rounded-md text-gray-400 border-gray-300 text-sm transition focus-within:text-gray-800 focus:border-blue-600 focus:ring-blue-600 disabled:cursor-not-allowed disabled:bg-gray-200 disabled:opacity-75">
+                            <select id="semestre" name="semestre" className="block w-full h-11 rounded-md text-gray-400 border-gray-300 text-sm transition focus-within:text-gray-800 focus:border-blue-600 focus:ring-blue-600 disabled:cursor-not-allowed disabled:bg-gray-200 disabled:opacity-75" onChange={(e)=>setUsuarioSemestre(e.target.value)} value={usuarioSemestre}>
                                 <option defaultValue="">Elegir Semestre </option>
                                 <option>Primero</option>
+                                <option>Segundo</option>
+                                <option>Tercero</option>
+                                <option>Cuarto</option>
+                                <option>Quinto</option>
+                                <option>Sexto</option>
+                                <option>Séptimo</option>
+                                <option>Octavo</option>
+                                <option>Noveno</option>
+                                <option>Decimo</option>
+                                <option>Onceavo</option>
                             </select>
                         </div>
                     </div>
 
                     <div className="flex items-center justify-center my-10">
                         <Link href="/registerFinish">
-                            <a className="btn-primary">Registrarme</a>
+                            <a className="btn-primary" onClick={()=>addUser()}>Registrarme</a>
                         </Link>
                     </div>
                 </form>
